47c8c601a979c89aaeb93f771f82c447d734414b
unknown <peter.ostertag99@gmail.com>
2023-09-25 17:28:41 -0500

    Add App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-// Might need more imports
+
+import Header from './Header';
+import Footer from './Footer';
+import AboutMe from './AboutMe';
+import Portfolio from './Portfolio';
+import Contact from './Contact';
+import Resume from './Resume';
 
 function App() {
-    // Content
+    return (
+        <Router>
+            <div className="app">
+                <Header />
+                <Switch>
+                    <Route path="/portfolio" component={Portfolio} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/resume" component={Resume} />
+                    <Route path="/" component={AboutMe} />
+                </Switch>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
