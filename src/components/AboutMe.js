3f3c6da84aee18951ab0f3c2b95776882e1d7920
unknown <peter.ostertag99@gmail.com>
2023-09-25 14:52:54 -0500

    Add AboutMe.js component

---
+++ src/components/AboutMe.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function AboutMe() {
+    return (
+        <section id="about-me">
+            <div className="about-me-content">
+                <h2>About Me</h2>
+                <p>
+                    Hello, I'm a web developer that is passionate about creating web applications and learning new technologies.
+                </p>
+            </div>
+        </section>
+      );
+}
+
+export default AboutMe;
