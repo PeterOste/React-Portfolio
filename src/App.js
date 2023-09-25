a2d6caf01943aec160137892feb70c290c3c1ffc
unknown <peter.ostertag99@gmail.com>
2023-09-25 17:47:52 -0500

    Fix file pathing in App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-import Header from './Header';
-import Footer from './Footer';
-import AboutMe from './AboutMe';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
-import Resume from './Resume';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
     return (
