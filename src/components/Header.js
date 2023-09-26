bb10920339acfb4d7e67191faacd59f706d1ca43
unknown <peter.ostertag99@gmail.com>
2023-09-26 13:20:58 -0500

    Fixed broken nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
@@ -7,10 +8,10 @@ function Header() {
                 <h1>Peter Ostertag</h1>
                 <nav>
                     <ul>
-                        <li><a href="#about-me">About Me</a></li>
-                        <li><a href="#portfolio">Portfolio</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li><a href="#resume">Resume</a></li>
+                        <li><Link to="/">About Me</Link></li>
+                        <li><Link to="/portfolio">Portfolio</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                        <li><Link to="/resume">Resume</Link></li>
                     </ul>
                 </nav>
             </div>
