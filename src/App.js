3f496db39e6107c78203025bbd6ab86898058f67
unknown <peter.ostertag99@gmail.com>
2023-09-25 23:00:50 -0500

    Fix style padding

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
-import Resume from './components/Resume';
-
+import Resume from './components/Resume'; 
 function App() {
     return (
         <Router>
