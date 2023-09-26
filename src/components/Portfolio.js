a1c1506b81e19fcd9f02d5c7113730cb91a784c0
unknown <peter.ostertag99@gmail.com>
2023-09-26 17:54:27 -0500

    Add Portfolio.js

---
+++ src/components/Portfolio.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <section id="portfolio" className="route-container">
+      <div className="portfolio-container">
+        <h2>Portfolio</h2>
+        {/* Add portfolio items here */}
+      </div>
+    </section>
+  );
+}
+
+export default Portfolio;
