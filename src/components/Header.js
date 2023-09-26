b7682790135c1d9f64947e016108052b797535cf
unknown <peter.ostertag99@gmail.com>
2023-09-26 17:45:18 -0500

    Add hover and select tab

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 function Header() {
     const location = useLocation();
-  
+    const [activeTab, setActiveTab] = useState(location.pathname);
+
     return (
-      <header>
-        <div className="header-container">
-          <h1>Peter Ostertag</h1>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/" className={location.pathname === '/' ? 'active' : ''}>
-                  About Me
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/portfolio"
-                  className={location.pathname === '/portfolio' ? 'active' : ''}
-                >
-                  Portfolio
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/contact"
-                  className={location.pathname === '/contact' ? 'active' : ''}
-                >
-                  Contact
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/resume"
-                  className={location.pathname === '/resume' ? 'active' : ''}
-                >
-                  Resume
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
+        <header>
+            <div className="header-container">
+                <h1>Peter Ostertag</h1>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link
+                                to="/"
+                                className={activeTab === '/' ? 'active' : ''}
+                                onClick={() => setActiveTab('/')}
+                            >
+                                About Me
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/portfolio"
+                                className={activeTab === '/portfolio' ? 'active' : ''}
+                                onClick={() => setActiveTab('/portfolio')}
+                            >
+                                Portfolio
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/contact"
+                                className={activeTab === '/contact' ? 'active' : ''}
+                                onClick={() => setActiveTab('/contact')}
+                            >
+                                Contact
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                to="/resume"
+                                className={activeTab === '/resume' ? 'active' : ''}
+                                onClick={() => setActiveTab('/resume')}
+                            >
+                                Resume
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     );
-  }
-  
-  export default Header;
-  
+}
+
+export default Header;
